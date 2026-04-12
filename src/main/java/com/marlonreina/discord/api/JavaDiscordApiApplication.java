0637a2afe5e4d4835712a8b49cd18f0117770853
ReinaMarlon <reinadinas.marlon@gmail.com>
2026-04-11 23:54:14 -0500

    Initial commit

---
+++ src/main/java/com/marlonreina/discord/api/JavaDiscordApiApplication.java
@@ -0,0 +1,13 @@
+package com.marlonreina.discord.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDiscordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDiscordApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlonreina/discord/api/JavaDiscordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlonreina.discord.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDiscordApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
