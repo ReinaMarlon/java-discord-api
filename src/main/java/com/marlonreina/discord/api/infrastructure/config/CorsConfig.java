72e6f327bdbb9941fbd5fd321b114f00c689311e
ReinaMarlon <reinadinas.marlon@gmail.com>
2026-04-15 20:50:48 -0500

    Cors configuration added in SpringSecurity.

--- src/main/java/com/marlonreina/discord/api/infrastructure/config/CorsConfig.java
+++ src/main/java/com/marlonreina/discord/api/infrastructure/config/CorsConfig.java
@@ -3,7 +3,12 @@ package com.marlonreina.discord.api.infrastructure.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -12,16 +17,18 @@ public class CorsConfig {
     public String frontendBaseUrl;
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
+    public CorsConfigurationSource corsConfigurationSource() {
 
-                registry.addMapping("/**")
-                        .allowedOrigins(frontendBaseUrl)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowCredentials(true);
-            }
-        };
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of(frontendBaseUrl));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
